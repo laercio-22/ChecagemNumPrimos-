1ea97c2ddc60b537e9de8acef35cea5e16fa31ef
Fulano de Tal <Fulano de Tal>
2018-11-06 08:01:42 -0200

    primeiro commit

---
+++ ChecagemNumPrimos/src/Main.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	
+	Scanner ler = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Informe um número");
+		
+		
+	}
+
+}
